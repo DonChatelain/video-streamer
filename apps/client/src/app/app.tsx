201594ce7cffbaa8d83b6823f27af05cdd29471a
Donald Chatelain <djchatelain@gmail.com>
2023-08-14 12:14:16 -0600

    viewToggle added to searchParams to retain state

--- apps/client/src/app/app.tsx
+++ apps/client/src/app/app.tsx
@@ -1,7 +1,10 @@
+import { Link } from 'react-router-dom';
+
 export function App() {
   return (
     <div>
       <h1>App</h1>
+      <Link to={'/videos'}>Videos</Link>
     </div>
   );
 }
--- apps/client/src/app/video-list/video-list.tsx
+++ apps/client/src/app/video-list/video-list.tsx
@@ -1,15 +1,9 @@
 import { Suspense, useEffect, useRef, useState } from 'react';
 import styles from './video-list.module.css';
-import {
-  Link,
-  ScrollRestoration,
-  useParams,
-  useSearchParams,
-} from 'react-router-dom';
+import { Link, useSearchParams } from 'react-router-dom';
 import { VideoListItem } from '@org/video-repo';
 import { humanFileSize } from '../util';
-import { useScroll, useScrollIntoView } from '@react-hooks-library/core';
-import { useQuery, useQueryClient } from '@tanstack/react-query';
+import { useQuery } from '@tanstack/react-query';
 
 /* eslint-disable-next-line */
 export interface VideoListProps {}
@@ -23,7 +17,15 @@ async function getNrop(): Promise<{ files: VideoListItem[] }> {
 
 export function VideoList(props: VideoListProps) {
   const box = useRef<HTMLDivElement | null>(null);
-  const [viewToggle, setViewToggle] = useState<'movies' | 'videos'>('movies');
+  const [searchParams, setSearchParams] = useSearchParams();
+  const queryParams = new URLSearchParams(searchParams);
+  const [viewToggle, setViewToggle] = useState<string>(
+    queryParams.get('viewToggle') || 'movies'
+  );
+
+  useEffect(() => {
+    setSearchParams({ viewToggle: viewToggle });
+  }, [viewToggle, setSearchParams]);
 
   const movieQuery = useQuery({
     queryKey: ['movies'],
@@ -42,12 +44,28 @@ export function VideoList(props: VideoListProps) {
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <div className={styles.controls}>
-        <button value={'movies'} onClick={() => setViewToggle('movies')}>
+        <button
+          value={'movies'}
+          className={viewToggle === 'movies' ? styles.buttonActive : ''}
+          onClick={() => setViewToggle('movies')}
+        >
           Movies
         </button>
-        <button value={'videos'} onClick={() => setViewToggle('videos')}>
+        <button
+          value={'videos'}
+          className={viewToggle === 'videos' ? styles.buttonActive : ''}
+          onClick={() => setViewToggle('videos')}
+        >
           Videos
         </button>
+      </div>
+      <div className={styles.stats}>
+        {viewToggle === 'movies' && (
+          <span>{movieQuery.data?.files.length + ' movies'}</span>
+        )}
+        {viewToggle === 'videos' && (
+          <span>{videoQuery.data?.files.length + ' videos'}</span>
+        )}
       </div>
       <div className={styles.movieList} ref={box}>
         {viewToggle === 'movies' &&
